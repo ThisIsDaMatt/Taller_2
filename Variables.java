a9b66320e47a9025b49e05d8c8f4fe678cac8ee1
Matias Munera <matiasmunera08@gmail.com>
2026-08-12 08:43:20 -0500

    Se añadieron 14 variables

--- Variables.java
+++ Variables.java
@@ -1,3 +1,19 @@
 public class Variables {
-    
+    public static void main(String[] args) {
+        byte Byte = 5;
+        byte Byte2 = -128;
+        short Short = 1248;
+        short Short2 = -29837;
+        int Integer = 20;
+        int Integer2 = -834;
+        long Long = 964686;
+        long Long2 = 999999999999999L;
+        float Float = 1.25345f;
+        double Double = 3.29347;
+        boolean Boolean = false;
+        boolean Boolean2 = true;
+        char Char = '\u263a';
+        String string = "Hola Mundo";
+
+    }
 }
