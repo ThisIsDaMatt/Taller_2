bc145748d6fa3f7e157d0ce6c9ee3a55fc77975a
Matias Munera <matiasmunera08@gmail.com>
2026-08-12 09:32:43 -0500

    Se empezó a trabajar en las ecuaciones

--- Ecuaciones.java
+++ Ecuaciones.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Ecuaciones {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x;
+        double y;
+        double z;
+        int opcion;
+
+        System.out.print("Ingrese un valor para x: ");
+        x = sc.nextDouble();
+        System.out.print("Ingrese un valor para z: ");
+        z = sc.nextDouble();
+        System.out.print("Elija una ecuación:" + "\n" + "- Ecuación 1" + "\n" + "- Ecuación 2");
+        opcion = sc.nextInt();
+
+        switch (opcion) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("Opción inválida, intente de nuevo.");
+        }
+    }    
 }
