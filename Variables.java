17f5d059eddf3ae840ba3a8887a7b4c2706f650d
Jeronimo Arcila <jeronimoarcila.3a@gmail.com>
2026-08-12 08:55:17 -0500

    Mas 6 variables

--- Variables.java
+++ Variables.java
@@ -6,14 +6,24 @@ public class Variables {
         short Short2 = -29837;
         int Integer = 20;
         int Integer2 = -834;
+        int Integer3 = 777;
         long Long = 964686;
         long Long2 = 999999999999999L;
         float Float = 1.25345f;
+        float Float2 = 1.01010101f;
         double Double = 3.29347;
         boolean Boolean = false;
         boolean Boolean2 = true;
         char Char = '\u263a';
+        char Char2 = '\u266a';
+        char Char3= '\u266b';
         String string = "Hola Mundo";
+        String string2 = "Hola UPB"; 
+        String string3 = "Hola Maker Space";
+        
+          
+
+
 
     }
 }
